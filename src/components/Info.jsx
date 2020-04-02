4df05c92f36cadaeac5f139637f9d78349b20d49
Adel Senhadji <adelsenhadjii@gmail.com>
2020-04-02 14:49:13 +0200

    update

--- src/components/Info.jsx
+++ src/components/Info.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 
 export default function Info(props) {
+
 	return (
 		<div>
-			<p>Generations: {props.gens}</p>
+			<span style={{fontSize:30}} className="badge badge-info mt-3" >Generations: {props.gens}</span>
 		</div>
-	); 
+	);
+	
 }
