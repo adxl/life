1eda900ce686a36af2c154b002ed950c5ccb94f5
Adel Senhadji <adelsenhadjii@gmail.com>
2020-04-02 14:08:01 +0200

    add Navbar component

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Navbar() {
+	return (
+		<React.Fragment>
+			<nav className="navbar navbar-dark bg-dark">
+				<a className="navbar-brand" href="">Life</a>
+			</nav>
+		</React.Fragment>
+	);
+}
