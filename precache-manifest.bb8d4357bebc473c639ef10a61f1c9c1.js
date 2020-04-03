94200e7358c57416fd1ed7d1822618ef739a8fbb
Adel Senhadji <adelsenhadjii@gmail.com>
2020-04-03 21:09:23 +0200

    Updates

---
+++ precache-manifest.bb8d4357bebc473c639ef10a61f1c9c1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "484d5eb7392fcc2f12429006b4e15524",
+    "url": "/life/index.html"
+  },
+  {
+    "revision": "0eec9fbf29766f470c2a",
+    "url": "/life/static/css/2.f65acde2.chunk.css"
+  },
+  {
+    "revision": "1c0459d1d91688ccf800",
+    "url": "/life/static/css/main.5a3d69b2.chunk.css"
+  },
+  {
+    "revision": "0eec9fbf29766f470c2a",
+    "url": "/life/static/js/2.c902da2c.chunk.js"
+  },
+  {
+    "revision": "ebc14457535110648808604cc4b96810",
+    "url": "/life/static/js/2.c902da2c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1c0459d1d91688ccf800",
+    "url": "/life/static/js/main.9d10e8a6.chunk.js"
+  },
+  {
+    "revision": "08918bb1896b410ca1b4",
+    "url": "/life/static/js/runtime-main.66b21700.js"
+  }
+]);
